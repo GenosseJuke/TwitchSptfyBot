90c3fc381777a4ea0911031e40bc06c968a9c71e
GenosseJuke <juke.genosse@gmail.com>
2025-04-08 15:26:19 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -3,24 +3,15 @@
 import config
 import spotify
 from twitchio.ext import commands
-from flask import Flask
-import threading
+from threading import Thread
+import asyncio
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=3000)
-
-
+# Deine Bot-Klasse
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(
-            token=config.TWITCH_OAUTH, 
-            prefix="!", 
+            token=config.TWITCH_OAUTH,
+            prefix="!",
             initial_channels=[config.TWITCH_CHANNEL]
         )
 
@@ -29,7 +20,6 @@ class Bot(commands.Bot):
 
     @commands.command(name="song")
     async def song(self, ctx):
-        # Aufruf der Funktion aus dem spotify-Modul
         current_song = spotify.get_current_song()
         await ctx.send(current_song)
 
@@ -38,13 +28,21 @@ class Bot(commands.Bot):
         response = spotify.add_song_to_queue(song_name)
         await ctx.send(response)
 
+# Instanz des Bots erstellen
+bot_instance = Bot()
 
-# Hauptprogramm starten
-if __name__ == "__main__":
-    # Flask im Hintergrund starten
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
+# Stelle sicher, dass die globale Variable für deinen Webhook gesetzt wird
+import eventsub_listener
+eventsub_listener.bot_instance = bot_instance
 
-    # Bot starten
-    bot = Bot()
-    bot.run()
+# Funktion, um den Flask-Server zu starten
+def run_flask():
+    from eventsub_listener import app
+    app.run(port=5000)
+
+# Starte den Flask-Server in einem separaten Thread
+flask_thread = Thread(target=run_flask)
+flask_thread.start()
+
+# Starte den Twitch-Bot
+bot_instance.run()
