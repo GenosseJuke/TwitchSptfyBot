8c1162591a7f36df4644d7ee8833132342a47007
GenosseJuke <juke.genosse@gmail.com>
2025-04-10 16:45:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ TWITCH_CHANNEL = "GenosseJuke"    # Dein Kanalname, ohne "#"
 # Spotify-Konfiguration
 SPOTIFY_CLIENT_ID = "d709a127c280453881fa2d340456892c"  # Hol dir die Client-ID von Spotify Developer Dashboard
 SPOTIFY_CLIENT_SECRET = "4c9767fc4a7f4248bfc2b386943ad50d"  # Spotify Client Secret
-SPOTIFY_REDIRECT_URI = "http://localhost:8888/callback"
+SPOTIFY_REDIRECT_URI = "https://jukebot.onrender.com"
   # Deine Redirect-URI
