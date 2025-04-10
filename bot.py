88781ccb7902ba1d129a8175fd8aa0c5e9e1f0c6
GenosseJuke <juke.genosse@gmail.com>
2025-04-10 17:04:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,12 +16,13 @@ class Bot(commands.Bot):
     @commands.command(name="song")
     async def song(self, ctx):
         current_song = spotify.get_current_song()
-        await ctx.send(f"Aktuell läuft: {current_song}")
+        await ctx.send(current_song)
 
     @commands.command(name="sr", aliases=["wunsch", "songrequest"])
     async def sr(self, ctx, *, song_name):
         response = spotify.add_song_to_queue(song_name)
-        await ctx.send(f"Song '{song_name}' wurde zur Warteschlange hinzugefügt!")
+        await ctx.send(response)
+
 
 if __name__ == "__main__":
     bot = Bot()
